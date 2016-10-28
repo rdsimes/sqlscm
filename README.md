b2a9f513dc8b00225b63f884c55fc45255ddccaf
Richard Simes <rdsimes@gmail.com>
2016-10-28 21:03:48 +1300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# sqlscm
-Source Code Management tool for SQL Databases
+# ScumDB
+ScumDB is a tool for managing database changes through your source code version control system.
+
+It's a bit like a migration frame work, however:
+
+* changes are SQL based - you dont need to learn a new syntax
+* Database Version numbers are the same as mercurial (or git) revision Ids
+* SProc and UDF definitions are checked in just like regular source code functions
+
+## Usage ##
+
+### Installation
+
+```
+npm install scumdb --save-dev
+```
+or
+```
+npm install scumdb --global
+```
+
+### Initialise scum
+```
+scum init
+> Conection string lookup method - requires sa or ddladmin rights? (env)
+> Connection string environment variable? (scumconnection)
+> Directory to store database changes in? (./Database)
+> Directory does not exist - create? (y)
+> Pattern to match Stored Procedures (./Database/StoredProcedures/*.sql)
+> Creating database structure and saving initial version [done]
+> settings saved to scum.json [done]
+```
+
+### Create a new change/migration
+
+```
+scum create [changename]
+```
+
+This will create 2 new files: 
+```
+/Database/changes/changename-up.sql
+/Database/changes/changename-down.sql
+```
+Then put in your required table creation, data migration or other DDL changes into the new script
+and commit them to source control
+
+### Apply outstanding changes
+
+```
+scum update
+> current database version: 0
+> preparing update to version: 1
+> found: 1 change to upgrade: changename-up.sql 
+> 0 changes to downgrade.
+> 0 proc changes & 0 UDF changes.
+> applying changes.
+> done
+```
+
+### Apply a specific version
+```
+hg update 0
+scum update
+> current database version: 1
+> preparing update to version: 0
+> found: 0 changes to upgrade. 
+> 1 change to downgrade: changename-down.sql
+> 0 proc changes & 0 UDF changes.
+> applying changes.
+> done
+```
+
+### Advance usage/options
+
+You may want to view the script before running - you can view that with:
+```
+scum script
+```
+
+Sometimes, for a particular migration you'll need to run UDF changes before DDL changes, other times you'll know you need them
+to run afterwards. to do this, just name your migration with a suffix of -before or -after.
+Before is the default, but this can be changed globally in scum.json
---
+++ index.js
@@ -0,0 +1,119 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('scumtest.db');
+var series = require('async/series');
+
+var mode = 'git'; // or 'hg'
+
+function findChanges(diffSummary){
+    var dbChanges = diffSummary
+        .split('\n')
+        .map(matchDbChangeInSummary)
+        .filter((changes) => changes && changes.length)
+        .map((change) =>  {
+            return { filePath: change[1], 
+                    additions: change[2].split('+').length-1, 
+                    deletions: change[2].split('-').length-1,
+                    isUp: change[1].endsWith('up.sql')
+                };
+            });
+    return dbChanges;
+}
+
+function matchDbChangeInSummary(diffSummaryLine){
+    /*
+    Finds the path to the sql file AND the indication of line adds/removes in the output from hg diff --stat
+    eg for input: ' Database/CreateFirstTable.up.sql   |   4 ++++' ] ]
+    it should capture the file path: 'Database/CreateFirstTable.up.sql',
+    and the plus symbols: '++++',
+    */
+    return / (.+.sql)[^-+]*([+-]+)/i.exec(diffSummaryLine);
+}
+
+
+function runChanges(downgrades, upgrades, sourceRevision, targetRevision){
+    //todo, downgrades need to check out the file in the source revision, while upgrades can use the target or working revision
+
+    upgrades.forEach((u) => fs.readFile(u.filePath, 'utf8', function(err, data) {
+        if (err) throw err;
+        console.log("Running: " + u.filePath);
+        db.exec(data);
+        db.run("INSERT INTO __ScumHistory(filePath, revision) values(?, ?)", [u.filePath, targetRevision]);
+        console.log("Done");
+    }));
+}
+
+function findCurrentRevision (cb){
+   return db.get("select revision from __ScumHistory order by timestamp desc limit 1", function(err, result){
+        if(err) return cb(null, null);
+        return cb(err, result);
+    });
+}
+
+function findTargetRevision(cb){
+    var cmd = mode == "git" ? "git rev-parse --short HEAD" : "hg parent";
+    return exec(cmd, (error, stdout, stderr) => {   
+        var revision = mode == "git" ?
+        stdout.trim() 
+        :stdout.split("\n")[0].split(":")[2];
+        return cb(null, revision);
+    });
+}
+
+var update = function(sourceRevision, targetRevision){
+    if (mode == "git" && sourceRevision == 0){
+        sourceRevision = "HEAD";
+    }
+    console.log("upgrade from revision:" + sourceRevision + " to " + targetRevision);
+    var cmd = mode == "git" ? 
+    "git diff --stat " + sourceRevision + " " + targetRevision
+    : 'hg diff --stat -r ' + sourceRevision + ":" + targetRevision;
+    exec(cmd, function(error, stdout, stderr) {
+        console.log(cmd, error, stdout, stderr);
+        var changes = findChanges(stdout);
+        var upgrades = changes.filter((c) => c.isUp && c.additions >= c.deletions);
+        var downgrades = changes.filter((c) => !c.isUp && c.additions < c.deletions);
+        
+        if (changes && changes.length > 0) {
+            console.log("DB Changes:");
+            console.log(downgrades.reduce(
+                (previous, current) => previous + "\n" + current.filePath, 
+                downgrades.length + " downgrades:"));
+            console.log(upgrades.reduce(
+                (previous, current) => previous + "\n" + current.filePath, 
+                upgrades.length + " upgrades:"));
+
+            runChanges(downgrades, upgrades, sourceRevision, targetRevision);
+            
+        } else 
+        {
+            console.log("no changes found - add and commit a migration to run it");
+        }
+    });
+};
+
+var findFirstRevision = function(cb){
+    if (mode == 'hg'){
+        return cb(null, 0);
+    }
+    return exec("git rev-list --max-parents=0 HEAD", cb);
+};
+
+series([findCurrentRevision, findTargetRevision, findFirstRevision], function (err, results){
+    current = results[0];
+    target = results[1];
+    first = results[2];
+    
+    if (current){
+        current = current.revision;
+    } else {
+        current = first;
+    } 
+
+    if (err){
+        console.log(err);
+        return;
+    }
+    update(current, target);
+});
